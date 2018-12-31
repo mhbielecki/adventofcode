1919d144d032303b3b56835f1889865c8a89fd52
Martin Bielecki <martin.bielecki@gmail.com>
2018-12-31 16:35:08 +0100

    [aoc2018] Exercise 5 - part 2

--- 2018/javascript/5.js
+++ 2018/javascript/5.js
@@ -3,17 +3,30 @@ var Stack = require('./Utils/Stack.js');
 
 fs.readFile('../input/5.txt', 'utf8', (err, contents) => {
     const polymer = contents.trim().split("");
-    const stack = new Stack();
 
+    const part1 = react(polymer).length();
+
+    const part2 = 'abcdefghijklmnopqrstuvwxyz'.split("").map(char => {
+        return react(polymer, char).length();
+    });
+
+    console.log(`Part 1: ${part1}`);
+    console.log(`Part 2: ${Math.min(...part2)}`);
+});
+
+const react = (polymer, problematicUnitChar) => {
+    const stack = new Stack();
     for (let i = 0, j = polymer.length; i < j; i++) {
+        if (problematicUnitChar && polymer[i].toLowerCase() === problematicUnitChar) {
+            continue;
+        }
         // Always 32 in diff between upper case and lower case of the same char (ascii)
-        if (stack.peek() && (Math.abs(stack.peek().charCodeAt() - polymer[i].charCodeAt()) === 32)) { 
+        if (stack.peek() && (Math.abs(stack.peek().charCodeAt() - polymer[i].charCodeAt()) === 32)) {
             stack.pop();
         }
         else {
             stack.push(polymer[i]);
         }
     }
-
-    console.log(`Part 1: ${stack.length()}`);
-});
+    return stack;
+};
