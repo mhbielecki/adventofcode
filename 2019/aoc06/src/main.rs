41a5ee1abc1b52a29a9047f66bea1341d176cf2a
Martin Bielecki <martin.bielecki@gmail.com>
2019-12-28 00:08:10 +0100

    [aoc2019] Day 6 part 2

--- 2019/aoc06/src/main.rs
+++ 2019/aoc06/src/main.rs
@@ -11,16 +11,47 @@ fn main() {
 
     for orbit in orbits.iter() {
         let s: Vec<&str> = orbit.split(')').collect();
-        let orbiters = objects.entry(s[0]).or_insert(Vec::new());
-        orbiters.push(s[1]);
+        let parent = objects.entry(s[0]).or_insert(Vec::new());
+        parent.push(s[1]);
+
+        // Comment for part 1
+        let child = objects.entry(s[1]).or_insert(Vec::new());
+        child.push(s[0]);
     }
 
-    println!("{}",rec("COM", 1, &objects));
+    //println!("Part 1: {}",part_1("COM", 1, &objects));
+    println!("Part 2: {}",part_2(&objects));
 }
 
-fn rec(n: &str, depth: usize, objects: &HashMap<&str, Vec<&str>>) -> usize {
+fn part_1(n: &str, depth: usize, objects: &HashMap<&str, Vec<&str>>) -> usize {
     match objects.get(n) {
-        Some(v) => (depth * v.len()) + v.iter().fold(0, |acc, x| acc + rec(x, depth+1, &objects)),
+        Some(v) => (depth * v.len()) + v.iter().fold(0, |acc, x| acc + part_1(x, depth+1, &objects)),
+        _ => 0
+    }
+}
+
+fn part_2(objects: &HashMap<&str, Vec<&str>>) -> usize {
+    let mut distances: HashMap<&str, usize> = HashMap::new();
+    let mut queue: Vec<(&str, usize)> = Vec::new();
+    queue.push(("YOU", 0));
+
+    while queue.len() > 0 {
+        let c = queue.remove(0);
+        if !distances.contains_key(c.0) {
+            distances.insert(c.0, c.1);
+            match objects.get(c.0) {
+                Some(v) => {
+                    for x in v {
+                        queue.push((x, c.1+1));
+                    }
+                }
+                _ => ()
+            };
+        }
+    }
+
+    match distances.get("SAN") {
+        Some(v) => *v - 2,
         _ => 0
     }
 }
