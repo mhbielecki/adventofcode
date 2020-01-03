f58c78cded4341e1344b518ced4c6480d428cdd6
Martin Bielecki <Martin.Bielecki@gyldendal.no>
2020-01-03 16:06:53 +0100

    [aoc2019] Day 8 part 2

--- 2019/aoc08/src/main.rs
+++ 2019/aoc08/src/main.rs
@@ -3,7 +3,6 @@ use std::fs;
 // The image is 25 pixels wide and 6 pixels tall.
 // Each layer is a 2D-array
 // Picture is a list of layers
-
 fn main() {
     let filename = "input/input.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file.");
@@ -17,7 +16,7 @@ fn main() {
         
     let chunk_size = 25 * 6;
     let layers: std::slice::Chunks<i32> = pixels.chunks(chunk_size);
-    let min = layers.into_iter().min_by_key(|x| count_digit(&x, 0));
+    let min = layers.clone().into_iter().min_by_key(|x| count_digit(&x, 0));
 
     //part 1: find the layer that contains the fewest 0 digits. On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
     match min {
@@ -25,13 +24,43 @@ fn main() {
             let ones = count_digit(&x,1);
             let twos = count_digit(&x,2);
             println!("Part 1: {}", ones * twos);
-
         },
         None => panic!("Error")
     };
+
+    //Part 2 - Decode image and show the secret password!
+    let mut final_layer: [[i32; 25]; 6] = [[2; 25]; 6];
+
+    for layer in layers {
+        let mut pixel_idx = 0;
+        for x in 0..6 {
+            for y in 0..25 {
+                if final_layer[x][y] == 2   {
+                    final_layer[x][y] = layer[pixel_idx];
+                }
+                pixel_idx = pixel_idx + 1;
+            }
+        }
+    }
+
+    for layer in final_layer.iter() {
+        pretty_print(&layer);
+    }
 }
 
 fn count_digit(layer: &[i32], digit: i32) -> usize {
     let digits: Vec<_> = layer.iter().filter(|&x| *x == digit).collect();
     digits.len()
+}
+
+fn pretty_print(layer: &[i32; 25]) {
+    for c in layer.iter() {
+        if *c == 0 {
+            print!("~");
+        } 
+        else {
+            print!("X");
+        }
+    }
+    println!();
 }
